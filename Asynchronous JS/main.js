bab8ce52dac6c4938f34f9ee01686c0ccb5922d7
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-17 18:33:16 -0300

    Question 2

--- Asynchronous JS/main.js
+++ Asynchronous JS/main.js
@@ -1,22 +1,40 @@
-function checaIdade(idade)
-{
-    return new Promise(function(resolve, reject) {
-        if(idade >= 18){
-            resolve("Maior que 18");
-        } else {
-            reject("Menor que 18");
-        }
-    });
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+var nomeUser;
+var repos =  [];
+
+function renderRepos() {
+    listElement.innerHTML = '';
+    for (repo of repos) {
+        var repoElement = document.createElement('li');
+        var repoText = document.createTextNode(repo);
+
+        repoElement.appendChild(repoText);
+        listElement.appendChild(repoElement)
+    }
 }
 
-setTimeout(() => {
-    checaIdade(19)
+renderRepos();
+
+function find() {
+    repos = [];
+    renderRepos();
+    
+    nomeUser = inputElement.value;
+
+    axios.get('https://api.github.com/users/'+ nomeUser +'/repos')
     .then(function(response) {
-        console.log(response);
+        console.log(response.data.length);
+
+        for(var i = 0; i < response.data.length; i++){
+            repos.push(response.data[i].name)
+        }
+        renderRepos();
     })
     .catch(function(error) {
         console.log(error);
     });
-}, 2000);
+}
 
-
+buttonElement.onclick = find;
