9451e70e67cd014597a08f7a3897fe30149b36c3
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-17 17:11:17 -0300

    Question 1

--- Asynchronous JS/main.js
+++ Asynchronous JS/main.js
@@ -1,7 +1,22 @@
-axios.get('https://api.github.com/users/diego3g')
+function checaIdade(idade)
+{
+    return new Promise(function(resolve, reject) {
+        if(idade >= 18){
+            resolve("Maior que 18");
+        } else {
+            reject("Menor que 18");
+        }
+    });
+}
+
+setTimeout(() => {
+    checaIdade(19)
     .then(function(response) {
         console.log(response);
     })
     .catch(function(error) {
-        console.warn(error);
+        console.log(error);
     });
+}, 2000);
+
+
