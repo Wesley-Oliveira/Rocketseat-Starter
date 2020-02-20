3097d5c595d53b5943dc3b5e58e201cc2b8b328c
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-20 19:54:16 -0300

    Template literals

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,62 +1,9 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+// Template literals
+var nomme = "Diego";
+var idade = 23; // Normalmente
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+console.log('Meu nome é ' + nome + 'e tenho ' + idade + ' anos'); // Com template literals
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-/// Operadores Rest e Spread
-// REST - serve para pegar o resto das propriedades
-// Em objeto:
-var usuario = {
-  nome: 'Diego',
-  idiade: 23,
-  empresa: 'Rocketseat'
-};
-
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
-
-console.log(nome);
-console.log(resto); // Em vetor
-
-var arr = [1, 2, 3, 4];
-var a = arr[0],
-    b = arr[1],
-    c = arr.slice(2);
-console.log(a);
-console.log(b);
-console.log(c); // Para parâmetros de funções:
-
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma(1, 3, 4)); // SPREAD - repassar as informações de um objeto/array para outro objeto/array
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3); // Usando para criar um novo objeto de um usuário com todas as propriedades porém mudando uma única propriedade
-
-var usuario1 = {
-  nome: 'Diego',
-  idiade: 23
-};
-
-var usuario2 = _objectSpread({}, usuario1, {
-  nome: 'Gabriel'
-});
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- ES6/main.js
+++ ES6/main.js
@@ -1,47 +1,10 @@
-/// Operadores Rest e Spread
+// Template literals
 
-// REST - serve para pegar o resto das propriedades
-// Em objeto:
-const usuario = {
-    nome: 'Diego',
-    idiade: 23,
-    empresa: 'Rocketseat'
-};
+const nomme = "Diego";
+const idade = 23;
 
-const { nome, ...resto } = usuario;
+// Normalmente
+console.log('Meu nome é ' + nome + 'e tenho ' + idade + ' anos');
 
-console.log(nome);
-console.log(resto);
-
-// Em vetor
-const arr = [1, 2, 3, 4];
-
-const [ a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// Para parâmetros de funções:
-function soma(...params){
-    return params.reduce( (total, next) => total + next);
-}
-
-console.log(soma(1, 3, 4));
-
-// SPREAD - repassar as informações de um objeto/array para outro objeto/array
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [ ...arr1, ...arr2];
-console.log(arr3);
-
-// Usando para criar um novo objeto de um usuário com todas as propriedades porém mudando uma única propriedade
-const usuario1 = {
-    nome: 'Diego',
-    idiade: 23,
-};
-
-const usuario2 = { ...usuario1, nome: 'Gabriel' };
-console.log(usuario2);
+// Com template literals
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
