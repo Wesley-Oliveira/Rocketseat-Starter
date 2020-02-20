a8ec246a65985795a89162c4d5a0fb3766cd752b
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-20 19:51:22 -0300

    Rest and spread operators (...)

--- ES6/main.js
+++ ES6/main.js
@@ -1,34 +1,47 @@
-// Desestruturação de objetos
+/// Operadores Rest e Spread
 
+// REST - serve para pegar o resto das propriedades
+// Em objeto:
 const usuario = {
     nome: 'Diego',
-    idade: 23,
-    endereco: {
-        cidade: 'Rio do Sul',
-        estado: 'SC',
-    },
+    idiade: 23,
+    empresa: 'Rocketseat'
 };
 
-// Normalmente
-// const nome = usuario.nome;
-// const idade = usuario.idade;
-// const cidade = usuario.endereco.cidade;
-
-// Com desestruturação
-const { nome, idade, endereco: { cidade } } = usuario;
+const { nome, ...resto } = usuario;
 
 console.log(nome);
-console.log(idade);
-console.log(cidade);
+console.log(resto);
 
-// Normalmente
-// function mostraNome(usuario) {
-//     console.log(usuario.nome);
-// }
+// Em vetor
+const arr = [1, 2, 3, 4];
 
-// Com desestruturação
-function mostraNome( { nome, idade }) {
-    console.log(nome, idade);
+const [ a, b, ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Para parâmetros de funções:
+function soma(...params){
+    return params.reduce( (total, next) => total + next);
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4));
+
+// SPREAD - repassar as informações de um objeto/array para outro objeto/array
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2];
+console.log(arr3);
+
+// Usando para criar um novo objeto de um usuário com todas as propriedades porém mudando uma única propriedade
+const usuario1 = {
+    nome: 'Diego',
+    idiade: 23,
+};
+
+const usuario2 = { ...usuario1, nome: 'Gabriel' };
+console.log(usuario2);
