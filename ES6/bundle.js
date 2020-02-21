d34a490806ce5e8832bccfe2ba05439f1e7cb918
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-20 21:51:54 -0300

    Question 2

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,64 +1,41 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var usuarios = [{
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Gabriel',
+  idade: 15,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Lucas',
+  idade: 30,
+  empresa: 'Facebook'
+}]; // Q2.1 Usando map - Crie uma variável que contenha todas idades dos usuários: [23, 15, 30]
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+var arrIdades = usuarios.map(function (item) {
+  return item.idade;
+});
+console.log(arrIdades); // Q2.2 Usando filter - Crie uma variável que tenha apenas os usuários que trabalham na Rocketseat e com mais de 18 anos: 
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+var arrUsuarios = usuarios.filter(function (item) {
+  if (item.idade > 18) return item;
+});
+console.log(arrUsuarios); // Q2.3 Usando find - Crie uma variável que procura por um usuário que trabalhe na empresa Google: undefined
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+var findEmpresa = usuarios.find(function (item) {
+  if (item.empresa == 'Google') return item.nome;
+});
+console.log(findEmpresa); // Q2.4 Unindo operações Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem no máximo 50 anos:
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Usuario =
-/*#__PURE__*/
-function () {
-  function Usuario(email, senha) {
-    _classCallCheck(this, Usuario);
-
-    this.email = email;
-    this.senha = senha;
+var arrMultiplicaIdade = usuarios.map(function (item) {
+  item.idade *= 2;
+  return item;
+});
+var arrFilterMultiplicaIdade = arrMultiplicaIdade.filter(function (item) {
+  if (item.idade < 50) {
+    return item;
   }
-
-  _createClass(Usuario, [{
-    key: "isAdmin",
-    value: function isAdmin() {
-      if (this.admin == true) return true;else return false;
-    }
-  }]);
-
-  return Usuario;
-}();
-
-var Admin =
-/*#__PURE__*/
-function (_Usuario) {
-  _inherits(Admin, _Usuario);
-
-  function Admin() {
-    var _this;
-
-    _classCallCheck(this, Admin);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Admin).call(this));
-    _this.admin = true;
-    return _this;
-  }
-
-  return Admin;
-}(Usuario);
-
-;
-var User1 = new Usuario('User@teste.com', 'senha123');
-var Adm1 = new Admin('Admin@teste.com', 'senha123');
-console.log(User1.isAdmin()); // false
-
-console.log(Adm1.isAdmin()); // true
+});
+console.log(arrFilterMultiplicaIdade);
--- ES6/main.js
+++ ES6/main.js
@@ -1,26 +1,35 @@
-class Usuario {
-    constructor( email, senha ) {
-        this.email = email;
-        this.senha = senha;
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+];
+
+// Q2.1 Usando map - Crie uma variável que contenha todas idades dos usuários: [23, 15, 30]
+const arrIdades = usuarios.map( item => item.idade);
+console.log(arrIdades);
+
+// Q2.2 Usando filter - Crie uma variável que tenha apenas os usuários que trabalham na Rocketseat e com mais de 18 anos: 
+const arrUsuarios = usuarios.filter( item => {
+    if(item.idade > 18)
+        return item;
+});
+console.log(arrUsuarios);
+
+// Q2.3 Usando find - Crie uma variável que procura por um usuário que trabalhe na empresa Google: undefined
+const findEmpresa = usuarios.find( item => {
+    if(item.empresa == 'Google')
+        return item.nome;
+});
+console.log(findEmpresa);
+
+// Q2.4 Unindo operações Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem no máximo 50 anos:
+const arrMultiplicaIdade = usuarios.map( (item) => {
+    item.idade *= 2;
+    return item;
+});
+const arrFilterMultiplicaIdade = arrMultiplicaIdade.filter( item => {
+    if(item.idade < 50){
+        return item;
     }
-
-    isAdmin() {
-        if(this.admin == true)
-            return true;
-        else
-            return false;
-    }
-}
-
-class Admin extends Usuario{
-    constructor() {
-        super();   
-        this.admin = true;
-    }
-};
-
-const User1 = new Usuario('User@teste.com', 'senha123');
-const Adm1 = new Admin('Admin@teste.com', 'senha123');
-
-console.log(User1.isAdmin()) // false
-console.log(Adm1.isAdmin()) // true
+});
+console.log(arrFilterMultiplicaIdade);
