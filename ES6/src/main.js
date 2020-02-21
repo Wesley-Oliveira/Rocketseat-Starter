c05fd529a25f93ff532c380dc4abf8b6df8167b1
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-21 15:53:06 -0300

    Question 1

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,1 +1,11 @@
-alert('Opa');
+// Q1
+import { Usuario as ClasseUsuario } from './functions';
+ClasseUsuario.info();
+
+// Q2
+import { idade } from './functions';
+alert(idade);
+
+// Q3
+import { Usuario, idade as idadeUsuario} from './functions';
+console.log(idadeUsuario);
