f1c17af45f1ed7971998d9cbb5b33c89c710133f
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-21 16:17:14 -0300

    Async/await

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,11 +1,34 @@
-// Q1
-import { Usuario as ClasseUsuario } from './functions';
-ClasseUsuario.info();
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('Ok')}, 2000);
+});
 
-// Q2
-import { idade } from './functions';
-alert(idade);
 
-// Q3
-import { Usuario, idade as idadeUsuario} from './functions';
-console.log(idadeUsuario);
+//Só pode usar um await se estiver dentro de uma função async
+//Com async - await
+async function executaPromise() {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+
+    //sem async -await
+    // minhaPromise().then(response => {
+    //     console.log(response);
+
+    //     minhaPromise().then(response => {
+    //         console.log(response);
+
+    //         minhaPromise().then(response => {
+    //             console.log(response);
+    //         })
+    //     })
+    // })
+}
+
+// É possível utilizar tmb com arrow function
+const executaPromise2 = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+
+executaPromise2();
