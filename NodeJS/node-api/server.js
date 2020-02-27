690af18b785b879c8ee0062e64f321bc09f765f9
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-27 20:36:23 -0300

    Creating structure

---
+++ NodeJS/node-api/server.js
@@ -0,0 +1,8 @@
+//importando o express
+const express = require("express");
+
+//executando a função express
+const app = express();
+
+//ouvindo na porta 3001, rodando na porta 3001
+app.listen(3001); 
