b213b78bad0ded548866c158893221ef6c0ffd02
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-27 19:19:39 -0300

    Final project: Render on screen

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
         
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list')
         this.registerHandlers();
     }
 
@@ -20,7 +21,34 @@ class App {
             html_url: 'https://github.com/Wesley-Oliveira/Rocketseat-Starter',
         });
 
-        console.log(this.repositories);
+        this.render()
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let tittleEl = document.createElement('strong');
+            tittleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(tittleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
