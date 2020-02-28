bb1e41848ef98a4ea6715aea51c83595568381ce
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-27 22:30:41 -0300

    Creating product model

--- NodeJS/node-api/server.js
+++ NodeJS/node-api/server.js
@@ -1,14 +1,28 @@
-//importando o express
+//importando o express e mongoose
 const express = require("express");
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
-//executando a função express
+// Iniciando o App
 const app = express();
+
+// Iniciando o DB
+mongoose.connect('mongodb+srv://admin:admin123@cluster0-gke9r.mongodb.net/nodeapi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+requireDir('./src/models');
+
+const Product = mongoose.model("Product");
 
 //Definindo rota raiz
 //req-> simboliza a requisição pro servidor (parâmetros, cabeçalho da requisição, usuário, ip)
 //res-> resposta que vai dar para a requisição
 app.get('/', (req, res) => {
-    res.send("Hello ");
+    Product.create({
+        title: "React teste",
+        description: "Testando",
+        url: "Teste"
+    });
+
+    return res.send("Hello ");
 })
 
 //ouvindo na porta 3001, rodando na porta 3001
