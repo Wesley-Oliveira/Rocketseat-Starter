4be1b8fc19ec10fca73e6639547935d7574d26b1
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-27 23:36:21 -0300

    Adding cors

--- NodeJS/node-api/server.js
+++ NodeJS/node-api/server.js
@@ -1,12 +1,15 @@
 //importando o express e mongoose
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 // Permite que eu envie informações no formato de json
 app.use(express.json());
+// Permite a api ser vista por outros dominios publicamente
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect('mongodb+srv://admin:admin123@cluster0-gke9r.mongodb.net/nodeapi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
