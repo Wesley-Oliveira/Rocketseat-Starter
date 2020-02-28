022d2d3622f1c655636b935a11096b8598bdfe97
Wesley-Oliveira <wesley0leby@gmail.com>
2020-02-27 22:53:17 -0300

    Restructuring of files

--- NodeJS/node-api/server.js
+++ NodeJS/node-api/server.js
@@ -8,22 +8,11 @@ const app = express();
 
 // Iniciando o DB
 mongoose.connect('mongodb+srv://admin:admin123@cluster0-gke9r.mongodb.net/nodeapi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// Usando a biblioteca require-dir para pegar todos os models e criar no bd
 requireDir('./src/models');
 
-const Product = mongoose.model("Product");
-
-//Definindo rota raiz
-//req-> simboliza a requisição pro servidor (parâmetros, cabeçalho da requisição, usuário, ip)
-//res-> resposta que vai dar para a requisição
-app.get('/', (req, res) => {
-    Product.create({
-        title: "React teste",
-        description: "Testando",
-        url: "Teste"
-    });
-
-    return res.send("Hello ");
-})
+// Rotas
+app.use("/api", require("./src/routes"));
 
 //ouvindo na porta 3001, rodando na porta 3001
 app.listen(3001); 
