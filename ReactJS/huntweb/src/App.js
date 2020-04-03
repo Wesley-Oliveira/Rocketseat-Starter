baa3ea9a9e8d2e9447d52e69564fe8bb1de809d5
Wesley-Oliveira <wesley0leby@gmail.com>
2020-04-02 21:39:53 -0300

    Creating header

--- ReactJS/huntweb/src/App.js
+++ ReactJS/huntweb/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from "./components/Header";
 
-class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <h1>Hello Rocketseat</h1>
-      </div>
-    );
-  }
-}
+import "./styles.css";
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+)
 
 export default App;
