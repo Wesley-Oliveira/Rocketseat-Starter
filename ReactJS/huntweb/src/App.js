bc3d614462ba430bd90d2e13f9a00e085a1ff321
Wesley-Oliveira <wesley0leby@gmail.com>
2020-04-02 22:04:34 -0300

    Fetching API products

--- ReactJS/huntweb/src/App.js
+++ ReactJS/huntweb/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Header from "./components/Header";
-
 import "./styles.css";
+
+import Header from "./components/Header";
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 )
 
